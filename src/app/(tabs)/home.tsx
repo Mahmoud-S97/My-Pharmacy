6227d5b7f977be8a27f9ace8788348197973683d
Mahmoud Saleh <mahmoudsaleh@RedmiS2-RedmiS2.localdomain>
2026-03-23 21:22:18 +0000

    Tabs(Tabs-Screens): Added some content for Tabs-Screens

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -4,8 +4,14 @@ import { View, Text } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View className='flex-1 bg-white'>
+      <View className='w-full flex flex-column gap-1 px-4 py-6 bg-purple-50 shadow'>
+        <Text className='text-start text-3xl text-purple-500 font-bold'>My Pharmacy</Text>
+        <Text className='text-start text-2xl text-black font-bold'>Home</Text>
+      </View>
+      <View className='flex-1 py-40 justify-center items-center'>
+        <Text className='text-3xl text-center font-bold'>Welcome <Text className='text-purple-500'>User!</Text></Text>
+      </View>
     </View>
   )
 }
