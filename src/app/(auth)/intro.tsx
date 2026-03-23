b8502121f34cce70f8daa46e7290df5528e33953
Mahmoud Saleh <mahmoudsaleh@RedmiS2-RedmiS2.localdomain>
2026-03-23 19:32:50 +0000

    Screen(Intro-Screen): Added Intro-Screen with custom Main-Button

--- src/app/(auth)/intro.tsx
+++ src/app/(auth)/intro.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
+import MainButton from '@/src/components/ui/globals/buttons/MainButton';
 
 const IntroScreen = () => {
+
     return (
-        <View className='flex-1 justify-center items-center bg-white'>
-            <Text className='text-4xl font-bold'>Intro Screen</Text>
+        <View className='flex-1 bg-white'>
+            <View className='flex-1 px-6 py-2 bg-white justify-center items-center'>
+              <Text className='text-5xl font-bold mb-20 text-center'>Welcome Back!</Text>
+              <View className='w-full h-[30%] flex flex-column gap-8 justify-center items-center'>
+                <MainButton title='Login' onPress={() => router.replace('/login')} />
+                <MainButton title='Sign Up' onPress={() => router.replace('/sign-up')} className='bg-white border' textClassName='text-gray-900' />
+              </View>
+            </View>
         </View>
     )
 }
---
+++ src/components/ui/globals/buttons/MainButton.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { cn } from '@/src/lib/nativeWindCss/cs';
+
+type MainButtonProps = {
+  children?: ReactNode,
+  title?: string,
+  className?: string,
+  textClassName?: string,
+  disabled?: boolean,
+  onPress?: () => void
+}
+
+const MainButton = ({ children, title, className, textClassName, disabled, onPress }: MainButtonProps) => {
+
+  const combinedClasses = disabled ? (className + ' bg-gray-400') : className;
+  const combinedTextClasses = disabled ? (textClassName + ' text-gray-200') : textClassName;
+
+  return (
+    <TouchableOpacity activeOpacity={0.7} disabled={disabled} onPress={onPress} className={cn('w-full h-[60] items-center justify-center bg-black rounded-full shadow', combinedClasses)}>
+      {children}
+      {title && <Text className={cn('text-lg text-center font-bold text-gray-50', combinedTextClasses)}>{title}</Text>}
+    </TouchableOpacity>
+  )
+}
+
+export default MainButton;
