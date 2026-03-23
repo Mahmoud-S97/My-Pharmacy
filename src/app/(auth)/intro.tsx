a94b6a536993dc3e0b660e2aa46d9ec0bd956006
Mahmoud Saleh <mahmoudsaleh@RedmiS2-RedmiS2.localdomain>
2026-03-23 18:45:36 +0000

    Dep(TailwindCSS): Set-up Tailwindcss(Nativewind)

--- src/app/(auth)/intro.tsx
+++ src/app/(auth)/intro.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 const IntroScreen = () => {
     return (
-        <View style={{ flex: 1 }}>
-            <Text>Intro Screen</Text>
+        <View className='flex-1 justify-center items-center bg-white'>
+            <Text className='text-4xl font-bold'>Intro Screen</Text>
         </View>
     )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,9 @@
-import 'react-native-reanimated';
-import { useEffect } from 'react';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import './global.css';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 SplashScreen.preventAutoHideAsync();
 
